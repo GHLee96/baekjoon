4afa8aacfd4563a500efea30db5680b98ce3687a
GHLee96 <87657436+GHLee96@users.noreply.github.com>
2024-03-28 01:11:36 +0900

    [Bronze I] Title: A/B - 2, Time: 48 ms, Memory: 31120 KB, Score: 2000 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.py
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.py
@@ -1,3 +1,10 @@
 a, b = map(int, input().split())
+print(a // b, end = '')
 
-print(a / b)
+if a % b:
+    print('.', end='')
+    i = 0
+    while a % b and i < 1000:
+        a = a % b * 10
+        i += 1
+        print(a // b, end = '')
